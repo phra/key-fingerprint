3e89c3898b487841a245f25f67a38c8d76c09a35
Francesco Soncina <phra@users.noreply.github.com>
2017-10-09 16:49:12 +0200

    INIT

---
+++ README.md
@@ -0,0 +1,2 @@
+# key-fingerprint
+Node.js library to easy calculate MD*, SHA*, etc fingerprint of a public/private key.
---
+++ index.ts
@@ -0,0 +1,33 @@
+import { createHash } from 'crypto'
+
+// tslint:disable-next-line:max-line-length
+const regex = /\n| |-----BEGIN CERTIFICATE-----|-----END CERTIFICATE-----|-----BEGIN PRIVATE KEY-----|-----END PRIVATE KEY-----|ssh-rsa|(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g
+
+export enum SUPPORTED_ALGORITHM {
+  BLAKE2B512 = 'blake2b512',
+  BLAKE2S256 = 'blake2s256',
+  GOST = 'gost',
+  MD4 = 'md4',
+  MD5 = 'md5',
+  RMD160 = 'rmd160',
+  SHA1 = 'sha1',
+  SHA224 = 'sha224',
+  SHA256 = 'sha256',
+  SHA384 = 'sha384',
+  SHA512 = 'sha512',
+}
+
+function _colons(fingerprintHex: string) {
+  return fingerprintHex.replace(/(.{2})(?=.)/g, '$1:')
+}
+
+export function fingerprint(
+  cert: string,
+  algorithm: SUPPORTED_ALGORITHM = SUPPORTED_ALGORITHM.SHA256,
+  colons: boolean = false,
+) {
+  const cleanKey = cert.replace(regex, '')
+  const buffer = new Buffer(cleanKey, 'base64')
+  const hash = createHash(algorithm).update(buffer).digest('hex')
+  return colons ? _colons(hash) : hash
+}
